0c52fbda46776ec263c4cd8a522bdf0f1e033222
Shreyadb18 <shreyadb.ise@gmail.com>
2026-09-15 23:39:28 +0530

    Variables,Data types
    Variables,Data types

---
+++ 1_first.js
@@ -0,0 +1,61 @@
+let fullName = "John Doe";
+const age = 21;
+let radius = 5;
+const a = 10;
+const price = 99.99;
+let is_Follow = true;
+let x = BigInt("123");
+let y = Symbol("Hello");
+typeof fullName;
+console.log(typeof fullName);
+typeof is_Follow;
+console.log(typeof is_Follow);
+typeof a;
+console.log(typeof a);
+
+console.log(fullName);
+console.log(age);
+console.log(radius);
+console.log(a);
+console.log(price);
+console.log(is_Follow);
+console.log("hello, World!");
+console.log(x);
+console.log(y);
+typeof x;
+console.log(typeof x);
+typeof y;
+console.log(typeof y);
+
+const student = {
+    fullName: "John Doe",
+    age: 21,
+    is_pass: true,
+    cgpa: 8.0,
+}
+
+student["name"] = "John smith";
+console.log(student);
+console.log(student.name);
+console.log(student.age);
+console.log(student.is_pass);
+typeof student;
+console.log(typeof student);
+
+
+const product = {
+    title : "Ball pen",
+    rating : 4.0,
+    offer : 5,
+    price : 270.0
+}
+console.log(product);
+
+const profile = {
+    userName : "Shradha",
+    is_Follow : true,
+    followers : 1000,
+    following : 500,
+};
+ console.log(profile);
+ console.log(typeof profile.followers);
