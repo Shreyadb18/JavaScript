7168f3116c634dca607ad11b476873a7548b16e9
Shreyadb18 <shreyadb.ise@gmail.com>
2026-09-25 19:45:01 +0530

    Loops : for loop
    for loop

---
+++ day4_script.js
@@ -0,0 +1,22 @@
+for(let i = 0; i<=5; i++){
+    console.log("Shreya");
+}
+
+for (let count = 0; count < 100000; count++) {
+    console.log("Shreya");
+}
+
+//Calculate sum of 1 to 5
+let sum = 0;
+for (let i = 1; i <= 5; i++) {
+    sum += i;//sum = 0 + 1
+}
+console.log("sum =  " + sum);
+console.log("loop has ended");
+
+//print 1 to 5
+
+for (let i = 1; i <= 5; i++) {
+    console.log("i = " + i);
+}
+
